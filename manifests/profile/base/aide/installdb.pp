9b818714fc968db50a4e06d700f03e42abbf69c5
lhinds <lhinds@redhat.com>
2017-05-31 21:20:32 +0100

    path corrections

--- manifests/profile/base/aide/installdb.pp
+++ manifests/profile/base/aide/installdb.pp
@@ -1,32 +1,36 @@
 # Initialises new database and copies it to the correct naming convention
-class tripleo::profile::base::aide::installdb ( {
-  exec { 'aide init':
-    command     => "/usr/sbin/aide --init --config /etc/aide.conf'",
-    user        => 'root',
-    refreshonly => true,
-    subscribe   => Concat['aide.conf']
-  }
+class tripleo::profile::base::aide::installdb (
+  $step = hiera('step'),
+  ) {
+    exec { 'aide init':
+      command     => "/usr/sbin/aide --init --config /etc/aide.conf'",
+      user        => 'root',
+      refreshonly => true,
+      subscribe   => Concat['aide.conf']
+    }
 
-  exec { 'install aide db':
-    command     => "/bin/cp -f /var/lib/aide/aide.db.new /var/lib/aide/aide.db",
-    user        => 'root',
-    refreshonly => true,
-    subscribe   => Exec['aide init']
-  }
+    exec { 'install aide db':
+      command     => "/bin/cp -f /var/lib/aide/aide.db.new /var/lib/aide/aide.db",
+      user        => 'root',
+      refreshonly => true,
+      subscribe   => Exec['aide init']
+    }
 
-  file { '/var/lib/aide/aide.db':
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    seluser => 'unconfined_u',
-    require => Exec['install aide db']
-  }
-  file { '/var/lib/aide/aide.db.new.gz':
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    seluser => 'unconfined_u',
-    require => Exec['aide init']
+    file { '/var/lib/aide/aide.db':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0600',
+      seluser => 'unconfined_u',
+      require => Exec['install aide db']
+    }
+
+    file { '/var/lib/aide/aide.db.new.gz':
+      owner   => root,
+      group   => root,
+      mode    => '0600',
+      seluser => 'unconfined_u',
+      require => Exec['aide init']
+    }
   }
 }
