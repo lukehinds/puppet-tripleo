ba879699b9c6a441ac1c512c1ad3700236eb1ae4
lhinds <lhinds@redhat.com>
2017-06-01 16:33:39 +0100

    path corrections

--- manifests/profile/base/aide.pp
+++ manifests/profile/base/aide.pp
@@ -60,7 +60,7 @@ class tripleo::profile::base::aide (
 
 # If a hash of rules is supplied with class then call auditd::rules
     if $rules {
-      create_resources(::tripleo::profile::base::aide::rules, $rules)
+      create_resources(tripleo::profile::base::aide::rules, $rules)
     }
     contain 'aide::cron'
   }
